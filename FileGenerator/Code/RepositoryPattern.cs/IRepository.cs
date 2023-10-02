d68a8b14826c18d6607dcf117579b31ef04abf46
Kunal Ahuja <imkunalahuja@gmail.com>
2023-10-02 13:21:33 +0530

    added source code

---
+++ FileGenerator/Code/RepositoryPattern.cs/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+// update YOUR_NAMESPACE with your namespace
+namespace YOUR_NAMESPACE 
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
